f74a44f0a3bde07168b4bda5ee6166569784edc3
betosousa <avss@cin.ufpe.br>
2016-11-24 08:49:32 -0300

    Camera
    Camera segue a mark.

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	public float t = 0.5f;
+
+	Transform player;
+	Vector3 offset;
+
+	void Start () {
+		player = GameObject.FindWithTag("Mark").GetComponent<Transform>();
+		offset = transform.position - player.position;
+	}
+	
+	void Update () {
+		transform.position = Vector3.Lerp(transform.position, player.position + offset, t);
+	}
+}
